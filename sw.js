5c5cca9abed3e3dc832be41e14189008f00908e2
DieW0W <62778310+DieW0W@users.noreply.github.com>
2020-06-02 14:38:28 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+function fetchPerson(id){
+
+    fetch(`http://swapi.dev/api/people/${id}/`)
+      .then( function(response){
+        return response.json()
+      })
+      .then(function(json){
+        console.log("data", json)
+  
+        if (!json.name){
+          return;
+        }
+  
+        const name = json.name;
+        const birth_year = json.birth_year;
+  
+  
+        const character = document.createElement('div');
+        character.className = "character";
+        character.textContent = name + ": " + birth_year;
+        //document.querySelector("#characters").appendChild(character)
+  
+        const html = `
+          <div class="character">
+            <div class="name">
+              <a href="${json.url}">${name}</a>
+            </div>
+            <div class="year">${birth_year}</div>
+          </div>
+          `
+        
+        document.querySelector("#characters").insertAdjacentHTML('afterbegin', html)  
+      })
+}
+  
+for (var i = 0; i <= 100; i++) {
+    fetchPerson(i)
+}
+
+
